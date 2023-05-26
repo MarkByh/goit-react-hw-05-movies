1ec2f0d7989043cc3d02e0308ee5ec87c2a53a1f
MarkByh <123231312fh@gmail.com>
2023-05-26 17:15:41 +0300

    k

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -3,16 +3,6 @@ import { Oval } from 'react-loader-spinner';
 export default function Loader() {
   return (
     <div style={{ margin: '0 auto' }}>
-      {/* <ThreeDots
-        height="150"
-        width="150"
-        radius="9"
-        color="tomato"
-        ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
-        wrapperClassName=""
-        visible={true}
-      /> */}
       <Oval
         height={80}
         width={80}
